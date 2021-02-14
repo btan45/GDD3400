2f62722c902b63b4d8d043f67c034aea466fddfd
Bri <briannatan45@gmail.com>
2021-02-13 22:35:09 -0700

    Created a simple game view
    Has constants file and game loop

--- GDD3400/GDD3400/GDD3400.py
+++ GDD3400/GDD3400/GDD3400.py
@@ -1,1 +1,21 @@
+import pygame
+import Constants
 
+pygame.init()
+clock = pygame.time.Clock()
+
+# display size
+screen = pygame.display.set_mode((Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT))
+
+# controls game loop
+isRunning = False
+
+while not isRunning:
+    screen.fill(Constants.BACKGROUND_COLOR)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = True
+
+    pygame.display.flip()
+    clock.tick(Constants.FRAME_RATE)
