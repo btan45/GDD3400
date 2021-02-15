26f97c340267a14a7bf89d5a9f0faa7f0e85c91b
Bri <briannatan45@gmail.com>
2021-02-14 20:39:48 -0700

    Implemented many enemies

--- GDD3400/GDD3400/GDD3400.py
+++ GDD3400/GDD3400/GDD3400.py
@@ -1,9 +1,15 @@
 import pygame
 import Constants
+import random
 from Vector import Vector
 from Player import Player
 from Enemy import Enemy
 from EnemyHunter import EnemyHunter
+
+def randomVector():
+    randomx = random.randint(0, Constants.WORLD_WIDTH)
+    randomy = random.randint(0, Constants.WORLD_HEIGHT)
+    return Vector(randomx, randomy)
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -16,8 +22,17 @@ isRunning = False
 
 # create player
 player = Player(Constants.PLAYER_POSITION, Constants.PLAYER_SPEED, Constants.PLAYER_SIZE, Constants.PLAYER_COLOR)
-enemy = Enemy(Constants.ENEMY_POSITION, Constants.ENEMY_SPEED, Constants.ENEMY_SIZE, Constants.ENEMY_COLOR)
-enemyHunter = EnemyHunter(Constants.ENEMY_POSITION, Constants.ENEMY_SPEED, Constants.ENEMY_SIZE, Constants.ENEMY_HUNTER_COLOR)
+
+enemies = []
+
+for i in range(Constants.NUM_ENEMIES):
+    enemy = Enemy(randomVector(), Constants.ENEMY_SPEED, Constants.ENEMY_SIZE, Constants.ENEMY_COLOR)
+    enemyHunter = EnemyHunter(randomVector(), Constants.ENEMY_SPEED, Constants.ENEMY_SIZE, Constants.ENEMY_HUNTER_COLOR)
+
+    enemies.append(enemy)
+    enemies.append(enemyHunter)
+
+
 
 while not isRunning:
     screen.fill(Constants.BACKGROUND_COLOR)
@@ -32,12 +47,9 @@ while not isRunning:
     player.draw(screen)
 
     # enemy
-    enemy.update(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT, player)
-    enemy.draw(screen, player)
-
-    # enemyHunter
-    enemyHunter.update(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT, player)
-    enemyHunter.draw(screen, player)
+    for enemy in enemies:
+        enemy.update(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT, player)
+        enemy.draw(screen, player)
 
     pygame.display.flip()
     clock.tick(Constants.FRAME_RATE)
